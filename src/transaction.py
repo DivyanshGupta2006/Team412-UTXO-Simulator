528fdbe5de5c67b0b4f2ceaa3c8c766bc02aca94
Harsh Mahajan <hrshvmhjn1102@gmail.com>
2026-02-01 01:07:31 +0530

    Added get_fees function to calculate the fees of a transaction

--- src/transaction.py
+++ src/transaction.py
@@ -19,3 +19,21 @@ class Transaction:
         }
         self.transaction_details["outputs"].append(new_output)
 
+    def get_fees(self, utxo_manager):
+        total_input = 0
+        for utxo in self.transaction_details["inputs"]:
+            prev_tx = utxo["prev_tx"]
+            index = utxo["index"]
+            owner = utxo["owner"]
+
+            utxo_owner = utxo_manager.get_owner_of_utxo(prev_tx, index)
+            if utxo_owner != owner:
+                raise ValueError("UTXO owner mismatch")
+            total_input += utxo_manager.get_value_of_utxo(prev_tx, index)
+
+        total_output = 0
+        for output in self.transaction_details["outputs"]:
+            total_output += output["amount"]
+
+        return total_input - total_output
+
